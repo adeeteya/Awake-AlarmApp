abfc71a7662215ca135251966c27408e966e3c6a
Aditya <adeeteya@gmail.com>
2025-07-07 21:07:15 -0400

    🖌️ add alarm refactor

--- lib/screens/add_alarm_screen.dart
+++ lib/screens/add_alarm_screen.dart
@@ -2,6 +2,7 @@ import 'package:awake/extensions/context_extensions.dart';
 import 'package:awake/services/alarm_cubit.dart';
 import 'package:awake/services/settings_cubit.dart';
 import 'package:awake/theme/app_colors.dart';
+import 'package:awake/widgets/time_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -168,6 +169,12 @@ class _AddAlarmScreenState extends State<AddAlarmScreen> {
             padding: const EdgeInsets.all(16),
             child: ListView(
               children: [
+                TimePickerWidget(
+                  initialTime: _selectedTime,
+                  onTimeChanged: (time) {
+                    print('Selected time: $time');
+                  },
+                ),
                 TextButton(
                   onPressed: _pickTime,
                   style: TextButton.styleFrom(
