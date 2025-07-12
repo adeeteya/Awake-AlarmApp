8458c8d6815914c42108d8392debc1af7a6ded15
Aditya <adeeteya@gmail.com>
2025-07-11 22:45:36 -0400

    ✨ added flavors setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,59 @@
-import 'package:awake/main_common.dart';
+import 'package:alarm/alarm.dart';
+import 'package:awake/app_router.dart';
+import 'package:awake/services/alarm_cubit.dart';
+import 'package:awake/services/alarm_database.dart';
+import 'package:awake/services/custom_sounds_cubit.dart';
+import 'package:awake/services/settings_cubit.dart';
+import 'package:awake/services/shared_prefs_with_cache.dart';
+import 'package:awake/theme/app_theme.dart';
+import 'package:device_preview_plus/device_preview_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
-Future<void> main() async {
-  await mainCommon();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferencesWithCache.initialize();
+  await AlarmDatabase.initialize();
+  await Alarm.init();
+
+  if (appFlavor == "development") {
+    runApp(DevicePreview(builder: (context) => const MyApp()));
+  } else {
+    runApp(const MyApp());
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static final GoRouter _router = createRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    final bool previewEnabled = DevicePreview.isEnabled(context);
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AlarmCubit()),
+        BlocProvider(create: (_) => SettingsCubit()),
+        BlocProvider(create: (_) => CustomSoundsCubit()),
+      ],
+      child: BlocBuilder<SettingsCubit, SettingsState>(
+        builder: (context, state) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            title: 'Awake- The Alarm Clock',
+            themeMode: state.mode,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            routerConfig: _router,
+            locale: previewEnabled ? DevicePreview.locale(context) : null,
+            builder: previewEnabled ? DevicePreview.appBuilder : null,
+          );
+        },
+      ),
+    );
+  }
 }
--- lib/main_development.dart
+++
@@ -1,5 +0,0 @@
-import 'package:awake/main_common.dart';
-
-Future<void> main() async {
-  await mainCommon(enableDevicePreview: true);
-}
