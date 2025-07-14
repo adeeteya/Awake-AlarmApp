f0347a0825e5c93b1b42bf9658c53e2e81905bd0
Aditya <adeeteya@gmail.com>
2025-07-14 04:37:11 -0400

    fix(android): ensure app appears on lock screen

--- android/app/src/main/kotlin/com/adeeteya/awake/MainActivity.kt
+++ android/app/src/main/kotlin/com/adeeteya/awake/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.adeeteya.awake
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+        }
+    }
+}
